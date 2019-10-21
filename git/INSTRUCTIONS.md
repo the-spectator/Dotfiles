102efd6aba9d21353d48d0423e4049d5354d95f6
Akshay Birajdar <akshayb@joshsoftware.com>
2019-10-21 18:06:49 +0530

    Add git dotfiles

---
+++ git/INSTRUCTIONS.md
@@ -0,0 +1,7 @@
+# INSTRUCTIONS FOR GIT CONFS
+
+
+## HOOKS
+### PRE-COMMIT Hook
+1. Copy pre-commit hooks .git/hooks of git project
+2. execute `sudo chmod +x .git/hooks/pre-commit`
---
+++ git/pre-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ~/colours.sh
+
+if [[ -z "$SKIP" ]]
+then
+  printf "\n${cyan}${bold}Running pre-commit hooks${nc}\n"
+  files=$(git status -s | grep -E 'A|M' | awk '{print $2}')
+  files="$files $(git status -s | grep -E 'R' | awk '{print $4}')"
+  echo $files | xargs rubocop --extra-details --parallel --force-exclusion
+  status=$?
+  if [ $status -eq 0 ]
+  then
+    printf "\n${cyan}Check Rubocop................................ ${green}${bold}Passed ✓${nc}\n"
+    exit 0
+  else
+    printf "\n${cyan}Check Rubocop................................ ${green}${bold}Failed ✗${nc}\n"
+    printf result >&2
+    exit 1
+  fi
+else
+  printf "\n${red}${bold}⚠ Skipping pre-commit hooks${nc}\n"
+fi
+printf "\n\n"
