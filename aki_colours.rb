ffa653d8a9f4f0931b3794d7e34981aeedc1995e
Akshay Birajdar <akshayb@joshsoftware.com>
2019-11-02 03:01:01 +0530

    Add ruby compatible colours

---
+++ aki_colours.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+def reset
+  "\033[0m"
+end
+
+def red
+  "\033[0;31m"
+end
+
+def black
+  "\033[0;30m"
+end
+
+def dark_grey
+  "\033[1;30m"
+end
+
+def lred
+  "\033[1;31m"
+end
+
+def green
+  "\033[0;32m"
+end
+
+def lgreen
+  "\033[1;32m"
+end
+
+def orange
+  "\033[0;33m"
+end
+
+def yellow
+  "\033[1;33m"
+end
+
+def blue
+  "\033[0;34m"
+end
+
+def lblue
+  "\033[1;34m"
+end
+
+def purple
+  "\033[0;35m"
+end
+
+def lPurple
+  "\033[1;35m"
+end
+
+def cyan
+  "\033[0;36m"
+end
+
+def lcyan
+  "\033[1;36m"
+end
+
+def lgray
+  "\033[0;37m"
+end
+
+def white
+  "\033[1;37m"
+end
+
+def bold
+  "\033[1m"
+end
