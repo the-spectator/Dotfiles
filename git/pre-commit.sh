c88393deba6645b9474be4db3e7dddbc469cbe9c
Akshay Birajdar <akshayb@joshsoftware.com>
2019-11-01 16:34:59 +0530

    Add comments and little bit of refactoring

--- git/pre-commit.sh
+++ git/pre-commit.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 source "$HOME/colours.sh"
 
-hooks=("rubocop_hook")
+# Array containing all checks/tasks we want to run in hook
+hooks=(
+  rubocop
+)
 
+# Function corresponding to check/task in hooks array
 function rubocop_hook() {
-  files=$(git diff --name-only --cached)
-  echo $files | xargs rubocop --extra-details --parallel --force-exclusion
+  local files status length
 
-  local status
+  # Get all the staged files except deleted files
+  files=$(git diff --name-only --cached --diff-filter=d)
+
+  # Get word-length of files
+  length=${#files}
+
+  # Run rubocop hook only when length is "not equal" to 0
+  if [[ length -ne 0 ]]; then
+    echo $files | xargs bundle exec rubocop --extra-details --parallel --force-exclusion
+  fi
+
+  # Get exit status of command executed
   status=$?
 
+  # Status is 0 when command exit status when successfully executed
   if [[ status -eq 0 ]]
   then
     printf "\n${cyan}Check ${bold}Rubocop${nc} ................................ ${nc}${green}${bold}Passed ✓${nc}\n"
@@ -20,12 +35,13 @@ function rubocop_hook() {
   fi
 }
 
+# Run hooks only when SKIP environment variable is not set
 if [[ -z "$SKIP" ]]
 then
   printf "\n${cyan}${bold}Running pre-commit hooks${nc}\n"
 
   for hook in "${hooks[@]}"; do
-    ${hook}
+    ${hook}_hook
   done
 else
   printf "\n${red}${bold}⚠  Skipping pre-commit hooks${nc}\n"
